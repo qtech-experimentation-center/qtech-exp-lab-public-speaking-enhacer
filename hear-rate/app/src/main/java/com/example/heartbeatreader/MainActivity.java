9a853b8026d79814828f0a269ef032152e6b2a84
Roberto Almeida <almeida.roberto7@gmail.com>
2020-06-23 20:59:17 -0500

    [Roberto] heart-beat: add button to start and stop the heart beat reader

--- hear-rate/app/src/main/java/com/example/heartbeatreader/MainActivity.java
+++ hear-rate/app/src/main/java/com/example/heartbeatreader/MainActivity.java
@@ -2,18 +2,40 @@ package com.example.heartbeatreader;
 
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends WearableActivity {
 
     private TextView mTextView;
+    private Button btnStartReader;
+    private boolean readerActivated = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextView = (TextView) findViewById(R.id.text);
+        mTextView = findViewById(R.id.textView);
+        btnStartReader = findViewById(R.id.btnStartReader);
+
+        btnStartReader.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(readerActivated == false) {
+                    mTextView.setText("Activated");
+                    readerActivated = true;
+                }
+                else {
+                    mTextView.setText("Desactivated");
+                    readerActivated = false;
+                }
+
+            }
+        });
 
         // Enables Always-on
         setAmbientEnabled();
